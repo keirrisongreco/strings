715610a0e09b8bda039d32e99661f51dd2b12ae6
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2025-10-22 08:10:48 -0300

    nova

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ function processaTexto(texto) {
 
     return palavras;
 }
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
+}
